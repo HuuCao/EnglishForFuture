a977d0e54d4323e82f9fc357108e0d140c148f31
HuuCao <58437506+HuuCao@users.noreply.github.com>
2022-12-12 15:20:26 +0700

    fix import excel

--- app/Imports/QuestionImports.php
+++ app/Imports/QuestionImports.php
@@ -15,19 +15,19 @@ class QuestionImports implements ToModel
     public function model(array $row)
     {
         return new Question([
-            'id' => $row[0],
-            'question_name' => $row[1],
-            'type' => (int)$row[2],
-            'image' => $row[3],
-            'autdio' => $row[4],
-            'answer_a' => $row[5],
-            'answer_b' => $row[6],
-            'answer_c' => $row[7],
-            'answer_d' => $row[8],
-            'correct_answer' => $row[9],
-            'explain' => $row[10],
-            'exam_id' => (int)$row[11],
-            'number_for_exam' => (int)$row[12],
+            // 'id' => $row[0],
+            'question_name' => $row[0],
+            'type' => (int)$row[1],
+            'image' => $row[2],
+            'autdio' => $row[3],
+            'answer_a' => $row[4],
+            'answer_b' => $row[5],
+            'answer_c' => $row[6],
+            'answer_d' => $row[7],
+            'correct_answer' => $row[8],
+            'explain' => $row[9],
+            'exam_id' => (int)$row[10],
+            'number_for_exam' => (int)$row[11],
         ]);
     }
 }
